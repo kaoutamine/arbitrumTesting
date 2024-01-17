50627a8aaeae58fd095cb48ed5e9e94b0cf152da
kaourintin tamine <kaourintin.tamine.personal@gmail.com>
2024-01-17 13:53:22 +0100

    improve the script arbddress

---
+++ arbitrumTesting/scripts/interactArbAdress.js
@@ -0,0 +1,56 @@
+const { Web3 } = require('web3');
+
+const { generateRandomAddressesAndKeys, generateAndSaveAddresses } = require('./adressGenerator');
+const web3 = new Web3('http://localhost:8547'); // Replace with your Arbitrum node URL
+
+
+const contractArtifact = require("../artifacts/contracts/ArbAdresses.sol/AddressTableInteraction.json");
+
+
+const contractABI = contractArtifact.abi;
+
+const contractAddress = '0xF7818cd5f5Dc379965fD1C66b36C0C4D788E7cDB'; // Replace with your contract address
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+
+async function registerAndLookup() {
+
+    
+    
+    const myAddress = "0x683642c22feDE752415D4793832Ab75EFdF6223c"
+
+    const address1 = "0x3f1Eae7D46d88F08fc2F8ed27FCb2AB183EB2d0E"
+    const unregisteredAddress = "0x683642c22feDE752415D4793832Ab75EFdF62231"
+
+    const regAdd1 = "0x1757340a42245e6Ee66E0a782B2372f3ADFC34DB"
+    const regAdd2 = "0xEA5aDDD0c3E0b2FB99f1313e0417A5a70EA917c5"
+
+    const startTime = Date.now();
+
+    console.log("starting the first call : get the adddress table size")
+    // Register addresses
+    const size = await contract.methods.getAddressTableSize().call();  
+    console.log(`table size: ${size} `);
+    console.log("starting the second call : register an address")
+    await contract.methods.registerAddresses([regAdd1]).send({ from: regAdd2 });
+    console.log("address registered")
+    // Lookup an address that exists
+    const indexKnown = await contract.methods.lookupAddress(regAdd1).call();
+
+
+    //lookup the address that has not been registered (how do i make new valid addresses????)
+    //
+    //const indexUknown = await contract.methods.lookupAddress(addressToLookup).call();
+
+    // Get address table size
+    const tableSize = await contract.methods.getAddressTableSize().call();  
+    console.log(`table size: ${tableSize} `);
+
+    const endTime = Date.now();
+
+    console.log(`Execution Time: ${endTime - startTime} ms`);
+    console.log(`Address Index: ${index}`);
+    console.log(`Address Table Size: ${tableSize}`);
+}
+
+registerAndLookup();
